011f78c69b3d14a9ee23b1ed4e6cec0e903b9a8f
Sophia <chida2001@mail.ru>
2023-11-14 19:57:49 +0300

    third

--- AQA_MTS/Program.cs
+++ AQA_MTS/Program.cs
@@ -6,10 +6,16 @@ namespace AQA_MTS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            /*Console.WriteLine("Enter name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello user, {name}");
+           */
+
+            bool isWhiteBreadFreash = true;
+            ushort whiteBreadPrice = 150;
+            ushort butterPrice = 230;
+            ushort milkPrice = 170;
+            float milkFat = 1.2F;
         }
     }
 }
