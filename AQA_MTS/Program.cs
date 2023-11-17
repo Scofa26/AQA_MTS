39ddc8b09718c63c9a85a57fc4e8efdd012f3706
Sophia <chida2001@mail.ru>
2023-11-18 00:02:50 +0300

    lesson7

--- AQA_MTS/Program.cs
+++ AQA_MTS/Program.cs
@@ -16,6 +16,60 @@ namespace AQA_MTS
             ushort butterPrice = 230;
             ushort milkPrice = 170;
             float milkFat = 1.2F;
+            ushort icecreamPrice = 350;
+
+            Console.Write("Введите начальную умму : ");
+            string? sumstring = Console.ReadLine();
+            short sum = Convert.ToInt16(sumstring);
+
+            if (sum > 0)
+            {
+               /* if (isWhiteBreadFreash)
+                {
+                    sum = Convert.ToInt16(sum - whiteBreadPrice);
+
+                }
+                else
+                {
+                    Console.WriteLine("Батон несвежий");
+                }*/
+
+                if(!isWhiteBreadFreash)
+                {
+                    Console.WriteLine("Батон несвежий");
+
+                }
+                else if(sum>= whiteBreadPrice)
+                {
+                    sum = Convert.ToInt16(sum - whiteBreadPrice);
+                    Console.WriteLine($"купили батон по цене {sum}");
+
+                }
+                else
+                {
+                    Console.WriteLine("На Батон нет денег");
+                }
+
+                sum = Convert.ToInt16(sum - butterPrice);
+
+                if (milkFat == 1.2F)
+                {
+                    sum = Convert.ToInt16(sum - milkPrice);
+                }
+
+                    if (sum >= 350)
+                {
+                    sum = Convert.ToInt16(sum - icecreamPrice);
+
+                }
+                Console.Write($"Остаток: {sum}");
+            }
+            else
+            {
+                Console.Write("Сумма недостаточная для покупки");
+
+            }
+
         }
     }
 }
