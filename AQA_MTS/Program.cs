20d307e34c40ae4079aae46bcad1a7db7773903d
Sophia <chida2001@mail.ru>
2023-11-19 22:10:52 +0300

    lesson 8 - Arrays

--- AQA_MTS/Program.cs
+++ AQA_MTS/Program.cs
@@ -6,68 +6,129 @@ namespace AQA_MTS
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Enter name");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hello user, {name}");
-           */
+            //int[] array = { 1, 9, 3, 6, 5 };
 
-            bool isWhiteBreadFreash = true;
-            ushort whiteBreadPrice = 150;
-            ushort butterPrice = 230;
-            ushort milkPrice = 170;
-            float milkFat = 1.2F;
-            ushort icecreamPrice = 350;
+            /*
+            int[] array1;
+            int n = 4;
+            array1 = new int[n];
 
-            Console.Write("Введите начальную умму : ");
-            string? sumstring = Console.ReadLine();
-            short sum = Convert.ToInt16(sumstring);
+            int[] array2 = new int[5] { 1, 9, 3, 6, 5 };
 
-            if (sum > 0)
+            //Console.WriteLine(array[4]);
+            //array[0] = 10;
+
+            Console.WriteLine($"Last item in array: {array[array.Length - 2]}");
+            Console.WriteLine($"Last item in array: {array[^2]}");
+            Console.WriteLine($"Last item in array: {array[array.GetUpperBound(0)]}");
+
+            Console.WriteLine(array[0]);
+            Console.WriteLine("Length: " + array.Length);                       // количество всех элементов
+            Console.WriteLine("Rank: " + array.Rank);                           // ранк массива
+            Console.WriteLine("GetLength(0): " + array.GetLength(0));           // количество строк
+            Console.WriteLine("GetUpperBound(0): " + array.GetUpperBound(0));   // Верхний индекс строк
+            Console.WriteLine("GetUpperBound(0): " + array.GetLowerBound(0));   // Нижний индекс строк
+
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-               /* if (isWhiteBreadFreash)
-                {
-                    sum = Convert.ToInt16(sum - whiteBreadPrice);
-
-                }
-                else
-                {
-                    Console.WriteLine("Батон несвежий");
-                }*/
-
-                if(!isWhiteBreadFreash)
-                {
-                    Console.WriteLine("Батон несвежий");
-
-                }
-                else if(sum>= whiteBreadPrice)
-                {
-                    sum = Convert.ToInt16(sum - whiteBreadPrice);
-                    Console.WriteLine($"купили батон по цене {sum}");
-
-                }
-                else
-                {
-                    Console.WriteLine("На Батон нет денег");
-                }
-
-                sum = Convert.ToInt16(sum - butterPrice);
-
-                if (milkFat == 1.2F)
-                {
-                    sum = Convert.ToInt16(sum - milkPrice);
-                }
-
-                    if (sum >= 350)
-                {
-                    sum = Convert.ToInt16(sum - icecreamPrice);
-
-                }
-                Console.Write($"Остаток: {sum}");
+                Console.Write($"{array[i]} ");
             }
-            else
-            {
-                Console.Write("Сумма недостаточная для покупки");
+            */
 
+            /*
+            int[] array = { 1, 9, 3, 6, 5 };
+
+            Console.WriteLine();
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = new Random().Next(100);
+            }
+
+            Console.WriteLine();
+            foreach (int item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            */
+
+            /*
+            int n = 10;
+            for (int i = n; i <= 10; i += 2)
+            {
+                if (i != 6)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.WriteLine();
+            string text = "Hello!!!";
+            foreach (char ch in text)
+            {
+                Console.Write($"{ch} ");
+            }
+
+            int[] arr1 = new int[6];
+
+            int u = 6;
+            bool flag = true;
+
+            do
+            {
+                if (u < 0) flag = false;
+
+                Console.WriteLine($"{u}");
+                u--;
+            } while (flag);
+
+            u = 6;
+            do
+            {
+                Console.WriteLine($"{u}");
+                u--;
+            } while (u >= 0);
+            */
+
+
+            // -============================================= Двумерный массив =============================================-
+
+            int[,] array = new int[4, 5];
+            Console.WriteLine("Length: " + array.Length);                       // количество всех элементов
+            Console.WriteLine("Rank: " + array.Rank);                           // ранк массива
+            Console.WriteLine("GetLength(0): " + array.GetLength(0));           // количество строк
+            Console.WriteLine("GetUpperBound(0): " + array.GetUpperBound(0));   // Верхний индекс строк
+            Console.WriteLine("GetLength(1): " + array.GetLength(1));           // количество столбцов
+            Console.WriteLine("GetUpperBound(1): " + array.GetUpperBound(1));   // Верхний индекс столбцов
+            Console.WriteLine("GetLowerBound(0): " + array.GetLowerBound(0));   // Нижний индекс строк
+            Console.WriteLine("GetLowerBound(1): " + array.GetLowerBound(1));   // Нижний индекс столбцов
+
+            Console.WriteLine("=========");
+
+            int t = 1;
+            // Задание значений
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = i + j;
+                }
+            }
+
+            int rows = array.GetLength(0);
+            int columns = array.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+            foreach (int item in array)
+            {
+                Console.Write($"{item} ");
             }
 
         }
