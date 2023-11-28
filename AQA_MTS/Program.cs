13ce2f7ab242e0b8eedb13267d8affabe2e2f21d
Sophia <chida2001@mail.ru>
2023-11-28 18:23:35 +0300

    fix HW добавила break в цикл ( строки 25-27) исправила нахождение среднего арифметического в строках 179 - 184

--- AQA_MTS/Program.cs
+++ AQA_MTS/Program.cs
@@ -10,79 +10,80 @@ namespace AQA_MTS
                  Создайте массив целых чисел. 
                  Напишете программу, которая выводит сообщение о том, входит ли заданное число в массив или нет.
                  Пусть число для поиска задается с консоли.*/
-
-            /* int[] array = new int[20];
-
-             for (int i = 0; i < array.Length; i++)
-             {
-                 array[i] = new Random().Next(100);
-                 Console.Write($"{array[i]}  ");
-             }
-             Console.Write("\n Введите число : ");
-             int number = Convert.ToInt32(Console.ReadLine());
-             bool flag = false;
-
-             for (int i = 0; i < array.Length; i++)
-             {
-                 if (array[i] == number) flag = true;
-             }
-
-             Console.WriteLine($"Входит ли {number} в массив ?  : {flag}");*/
-
             /*
-                        Задание 2
-                        Создайте массив целых чисел. Удалите все вхождения заданного числа из массива.
-                        Пусть число задается с консоли.Если такого числа нет -выведите сообщения об этом.
-                        В результате должен быть новый массив без указанного числа.*/
+                        int[] array = new int[20];
 
-            /* Console.Write("Введите размерность массива : ");
-             int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            array[i] = new Random().Next(100);
+                            Console.Write($"{array[i]}  ");
+                        }
+                        Console.Write("\n Введите число : ");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        bool flag = false;
 
-             int[] array1 = new int[n];
-             int[] array1_copy = new int[n-1];
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            if (array[i] == number) { flag = true; break; }
+                        }
 
-              for (int i = 0; i < n; i++)
-             {
-                 array1[i] = new Random().Next(100);
-                 Console.Write($"{array1[i]}  ");
-             }
+                        Console.WriteLine($"Входит ли {number} в массив ?  : {flag}");*/
 
-             Console.Write("\n Введите число : ");
-             int number1 = Convert.ToInt32(Console.ReadLine());
 
-             bool flag1 = false;
-             int j=0,k=0;
+            /* Задание 2
+                         Создайте массив целых чисел. Удалите все вхождения заданного числа из массива.
+                         Пусть число задается с консоли.Если такого числа нет -выведите сообщения об этом.
+                         В результате должен быть новый массив без указанного числа.
+ */
+           /* Console.Write("Введите размерность массива : ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-             for (int i = 0; i < n; i++)
-             {
-                 if (array1[i] == number1)
-                 {
-                     flag1 = true;
-                     j=i;
-                 }
+            int[] array1 = new int[n];
+            int[] array1_copy = new int[n - 1];
 
-             }
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = new Random().Next(100);
+                Console.Write($"{array1[i]}  ");
+            }
 
-             if (flag1 == true)
-             {
-                 for (int i = 0; i < n; i++)
-                 {
-                     if (i == j) continue;
-                     array1_copy[k] = array1[i];
-                     k++;
-                 }
+            Console.Write("\n Введите число : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
 
-                 Console.WriteLine("Новый массив:");
+            bool flag1 = false;
+            int j = 0, k = 0;
 
-                 for (int i = 0; i < array1.Length - 1; i++)
-                 {
+            for (int i = 0; i < n; i++)
+            {
+                if (array1[i] == number1)
+                {
+                    flag1 = true;
+                    j = i;
+                    break;
+                }
 
-                     Console.Write($" {array1_copy[i]}  ");
+            }
 
-                 }
-             }
-             else Console.Write($"Число {number1} не входит в массив");*/
+            if (flag1 == true)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == j) continue;
+                    array1_copy[k] = array1[i];
+                    k++;
+                }
 
+                Console.WriteLine("Новый массив:");
+
+                for (int i = 0; i < array1.Length - 1; i++)
+                {
+
+                    Console.Write($" {array1_copy[i]}  ");
+
+                }
+            }
+            else Console.Write($"Число {number1} не входит в массив");
+*/
             /* Задание 3
              Создайте и заполните массив случайным числами и выведете максимальное, минимальное и среднее значение.
              Для генерации случайного числа используйте метод Random().
@@ -145,77 +146,77 @@ namespace AQA_MTS
              Посчитайте среднее арифметическое элементов каждого массива и сообщите, для какого из массивов это значение оказалось
              больше(либо сообщите, что их средние арифметические равны).*/
 
-            /* int[] array3 = new int[5];
-             int[] array4 = new int[5];
+            int[] array3 = new int[5];
+            int[] array4 = new int[5];
 
-             for (int i = 0; i < 5; i++)
-             {
-                 array3[i] = new Random().Next(10);
-                 array4[i] = new Random().Next(10);
+            for (int i = 0; i < 5; i++)
+            {
+                array3[i] = new Random().Next(10);
+                array4[i] = new Random().Next(10);
+            }
+            Console.WriteLine("Массив 1 : ");
 
-             }  
-             Console.WriteLine("Массив 1 : ");
+            for (int i = 0; i < 5; i++)
+            {
 
-             for (int i = 0; i < 5; i++)
-             {
+                Console.Write($"{array3[i]} ");
 
-                 Console.Write($"{array3[i]} ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Массив 2 : ");
+            for (int i = 0; i < 5; i++)
+            {
 
-             }
-             Console.WriteLine("");
-             Console.WriteLine("Массив 2 : ");
-             for (int i = 0; i < 5; i++)
-             {
+                Console.Write($"{array4[i]} ");
 
-                 Console.Write($"{array4[i]} ");
+            }
+            int sum3 = 0, sum4 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum3 += array3[i];
+                sum4 += array4[i];
+            }
+            double mean1 = sum3 / 5.0;
+            double mean2 = sum4 / 5.0;
 
-             }
-             int sum3 = 0, sum4 = 0;
-             for(int i=0; i<5; i++)
-             {
-                 sum3 += array3[i];
-                 sum4 += array4[i];
-
-             }
-
-             if (Convert.ToDouble(sum3 / 2.0) == Convert.ToDouble(sum4 / 2.0)) Console.WriteLine("Средние арифметические равны ");
-             else if (Convert.ToDouble(sum3 / 2.0) > Convert.ToDouble(sum4 / 2.0)) Console.WriteLine("Для 1-го массива ср.а. больше ");
-             else Console.WriteLine("Для 2-го массива ср.а. больше ");*/
+            if (mean1 == mean2) Console.WriteLine("Средние арифметические равны ");
+            else if (mean1 > mean2) Console.WriteLine("Для 1-го массива ср.а. больше ");
+            else Console.WriteLine("Для 2-го массива ср.а. больше ");
 
             /*Задание 5
             Создайте массив из n случайных целых чисел и выведите его на экран.
             Размер массива пусть задается с консоли и размер массива может быть больше 5 и меньше или равно 10.
             Если n не удовлетворяет условию - выведите сообщение об этом.Если пользователь ввёл не подходящее число, то программа должна просить пользователя повторить ввод.
             Создайте второй массив только из чётных элементов первого массива, если они там есть, и вывести его на экран.*/
-            Console.WriteLine("Введите число");
-            int n1 = Convert.ToInt32(Console.ReadLine());
+            /* Console.WriteLine("Введите число");
+             int n1 = Convert.ToInt32(Console.ReadLine());
 
-            while( n1 > 10 || n1 <= 5 )
-            {
-                Console.WriteLine("Введенное число не принадлежит интервалу (5,10]. Введите новое число");
-                n1 = Convert.ToInt32(Console.ReadLine());
-            }
+             while (n1 > 10 || n1 <= 5)
+             {
+                 Console.WriteLine("Введенное число не принадлежит интервалу (5,10]. Введите новое число");
+                 n1 = Convert.ToInt32(Console.ReadLine());
+             }
 
-            int[] array5 = new int[n1];
-            int[] array6 = new int[n1];
-            int count = 0;
+             int[] array5 = new int[n1];
+             int[] array6 = new int[n1];
+             int count = 0;
 
-            for (int i = 0; i < n1; i++) array5[i] = new Random().Next(10);
+             for (int i = 0; i < n1; i++) array5[i] = new Random().Next(10);
 
-            Console.WriteLine("");
-            Console.WriteLine("Массив 1 : ");
-            for (int i = 0; i < n1; i++)  Console.Write($"{array5[i]} ");
-           
-            for (int i = 0; i < n1; i++)
-            {
+             Console.WriteLine("");
+             Console.WriteLine("Массив 1 : ");
+             for (int i = 0; i < n1; i++) Console.Write($"{array5[i]} ");
 
-                if (array5[i] % 2 == 0) { array6[count] = array5[i]; count++; }
+             for (int i = 0; i < n1; i++)
+             {
 
-            }
-            Console.WriteLine("");
-            Console.WriteLine("Массив 2 : ");
-            for (int i = 0; i <count; i++)  Console.Write($"{array6[i]} ");
-         
+                 if (array5[i] % 2 == 0) { array6[count] = array5[i]; count++; }
+
+             }
+             Console.WriteLine("");
+             Console.WriteLine("Массив 2 : ");
+             for (int i = 0; i < count; i++) Console.Write($"{array6[i]} ");*/
+
         }
     }
 }
