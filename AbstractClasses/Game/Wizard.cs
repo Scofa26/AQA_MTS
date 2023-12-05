554a7eccef8029b8dcfec942aa5b2c7179463b2d
Sophia <chida2001@mail.ru>
2023-12-05 19:17:16 +0300

    abstractClasses done

---
+++ AbstractClasses/Game/Wizard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses.Game
+{
+    public class Wizard : Character
+    {
+        public Wizard(int level) : base(level)
+        {
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("Warrior is attacking");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("Warrior is attacking");
+        }
+
+        public override void UseAbility()
+        {
+            Console.WriteLine("Warrior is attacking");
+        }
+    }
+}
