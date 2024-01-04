8e08c1fad889a1be35e376908beaf324156ca9b1
Sophia <chida2001@mail.ru>
2024-01-04 16:57:25 +0300

    updated

---
+++ Delegate/OperationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal enum OperationType
+    {
+        Add, Subtract, Multiply
+    }
+}
--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Delegate;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+
+        // -================= Single Cast Delegate
+          new Example1().Run();
+        // new Example2().Run();
+
+    }
+}
