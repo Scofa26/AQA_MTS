707959b583ce10aa6fcce1947ba12369c509887b
Sophia <chida2001@mail.ru>
2024-01-09 21:04:08 +0300

    part 2

---
+++ Delegate/Example7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Example7
+    {
+        delegate void Message();
+        delegate int Operation(int x, int y);
+
+        public void Run()
+        {
+            Message? msg = null;
+            //msg(); // ! Ошибка: делегат равен null
+            msg?.Invoke();
+
+            Operation op = Add;
+            int result1 = op.Invoke(1, 2);
+
+            op -= Add;
+            //int result2 = op.Invoke(2, 3); // ! Ошибка: делегат равен null
+            int? result3 = op?.Invoke(2, 3);
+        }
+
+        private void ShowMessage() => Console.WriteLine("Have fun!");
+        private int Add(int x, int y) => x + y;
+    }
+}
