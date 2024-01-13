67089186f8adf1c225e91c9adfb0508046ec5188
Sophia <chida2001@mail.ru>
2024-01-13 20:07:21 +0300

    update

---
+++ Task1_Delegate/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Task1_Delegate;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        new Task1().Run();
+    }
+}
---
+++ Task1_Delegate/Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Delegate
+{
+    internal class Task1
+    {
+        public delegate int ReturnInt();
+
+        public void Run()
+        {
+            ReturnInt returnint = PrintInt;
+            returnint.Invoke();
+        }
+        public int PrintInt()
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(1, 10);
+            Console.WriteLine(n);
+            return n;
+        }
+    }
+}
