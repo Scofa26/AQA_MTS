e033dcfaccb18531881b19476cba1ed3a49e7eeb
Sophia <chida2001@mail.ru>
2024-01-25 18:34:59 +0300

    update

---
+++ NUnitProject/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitProject
+{
+    internal static class Calc
+    {
+        public static int Sum(int x, int y) => x + y;
+    }
+}
---
+++ NUnitProject/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace NUnitProject
+{
+    
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
