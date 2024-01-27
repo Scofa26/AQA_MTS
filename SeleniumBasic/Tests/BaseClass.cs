fb879cbde11a4ba02ffb6c666c41db8029ace37d
Sophia <chida2001@mail.ru>
2024-01-27 13:30:12 +0300

    update

---
+++ SeleniumBasic/Tests/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using SeleniumBasic.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasic.Tests
+{
+    internal class BaseClass
+    {
+        protected IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void FactoryDriverTest()
+        {
+            Driver = new Browser().Driver;
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
