a8996814a3e40d6ecdb474d595997de2bb874dea
Sophia <chida2001@mail.ru>
2024-01-27 09:52:48 +0300

    update

---
+++ SeleniumBasic/Core/AdvancedDriver.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasic.Core
+{
+    internal class AdvancedDriver
+    {
+        private string basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public IWebDriver GetChromeDriver()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--incognito");
+            chromeOptions.AddArguments("--disable-gpu");
+            chromeOptions.AddArguments("--disable-extensions");
+            // chromeOptions.AddArguments("--headless");
+            chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            chromeOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);
+
+            return new ChromeDriver(basePath + @"/Resources/", chromeOptions);
+        }
+
+        public IWebDriver GetFireFoxDriver()
+        {
+            var mimeTypes =
+            "image/png,image/gif,image/jpeg,image/pjpeg,application/pdf,text/csv,application/vnd.ms-excel," +
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" +
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+            var ffOptions = new FirefoxOptions();
+            var profile = new FirefoxProfile();
+
+            profile.SetPreference("browser.download.folderList", 2);
+            profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", mimeTypes);
+            profile.SetPreference("browser.helperApps.neverAsk.openFile", mimeTypes);
+            ffOptions.Profile = profile;
+
+            ffOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            ffOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);
+
+            return new FirefoxDriver(ffOptions);
+        }
+    }
+}
---
+++ SeleniumBasic/Core/SimpleDriver.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasic.Core
+{
+    internal class SimpleDriver
+    {
+        public IWebDriver Driver
+        {
+            get
+            {
+                var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                //string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+                // return new ChromeDriver(@"C:\Users\Scofa\source\repos\AQA_MTS\SeleniumBasic\Recources\chromedriver.exe");
+               //  return new ChromeDriver(path + @"\Recources\");
+                 return new ChromeDriver(basePath + @"\Recources\");
+              
+            }
+        }
+    }
+}
