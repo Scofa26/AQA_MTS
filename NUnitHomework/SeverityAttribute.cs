e48b30a6b8090515eb24b64581b0da3e42a06e04
Sophia <chida2001@mail.ru>
2024-01-29 00:01:43 +0300

    done HW

---
+++ NUnitHomework/SeverityAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitHomework
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class SeverityAttribute : PropertyAttribute
+    {
+        public SeverityAttribute(SeverityLevel level)
+          : base(level.ToString()) { }
+    }
+}
---
+++ NUnitHomework/TestCasesForCalc.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitHomework
+{
+    [TestFixture, Author("Scofa")]
+    internal class TestCasesForCalc
+    {
+        [TestCase(10, 5, 2),
+         Category("Div int"),
+         Category("positive"),
+         Order(1),
+         Severity(SeverityLevel.Critical)]
+        public void Test1(int x, int y, int result)
+        {
+            Assert.That(Calc.Div(x, y), Is.EqualTo(result));
+        }
+
+        [TestCase(-2.4, 2.0, ExpectedResult = -1.2),
+         Category("Div double"),
+         Category("positive"),
+         Severity(SeverityLevel.Critical),
+         Order(2)]
+        public double Test2(double x, double y)
+        {
+            return Calc.Div(x, y);
+        }
+
+        [Test,
+            Category("Div double"),
+            Category("negative"),
+            Severity(SeverityLevel.Critical)
+           ]
+        public void Test3()
+        {
+            Assert.That(Double.IsInfinity(Calc.Div(10.0, 0.0)));
+        }
+
+        [Test,
+         Category("Div int"),
+         Category("negative"),
+         Severity(SeverityLevel.Critical)
+        ]
+
+        public void Test4()
+        {
+            var rez = Assert.Throws<DivideByZeroException>(() => Calc.Div(10, 0));
+            StringAssert.Contains("Attempted to divide by zero.", rez.Message.ToString());
+        }
+
+        [Test,
+         Ignore("Ignore this test "),
+         Category("Div double"),
+         Category("negative"),
+         Severity(SeverityLevel.Low)]
+        public void Test5(
+           [Values(6)] int x,
+           [Values(3)] int y,
+           [Values(2)] int result)
+        {
+            Assert.That(Calc.Div(x, y), Is.EqualTo(result));
+        }
+    }
+}
+
