a28870f688fc3df562aa1394427e029f7ba99bb5
Sophia <chida2001@mail.ru>
2024-03-18 07:54:55 +0300

    done

---
+++ ValueOfObjects/Tests/BaseSuite.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValueOfObjects.Core;
+
+namespace ValueOfObjects.Tests
+{
+    [SetUpFixture]
+    internal class BaseSuite
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        [OneTimeSetUp]
+        public static void SuiteSetup()
+        {
+            new NLogConfig().Config();
+            Logger.Info("NLog setted up.");
+        }
+    }
+}
